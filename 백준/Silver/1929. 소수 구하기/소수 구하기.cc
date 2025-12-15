9188691aa30c00e6ee792e1b8d977a10bd0a0a6b
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2025-12-15 20:09:47 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -5,15 +5,27 @@ using namespace std;
 // 소수 구하기
 // 에라토스테네스의 체 사용
 
-// 소수 판별
-bool isPrime(int n)
-{
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0) return false;
+// 에라토스테네스의 체 알고리즘 구현
+vector<bool> sieveOfEratosthenes(int maxNum) {
+	// true이면 소수, false이면 소수가 아님
+    vector<bool> isPrime(maxNum + 1, true);
+    isPrime[0] = false; 
+    isPrime[1] = false; // 0과 1은 소수가 아님
+
+	// 에라토스테네스의 체 알고리즘 적용
+	// 2부터 maxNum의 제곱근까지 반복
+    for (int i = 2; i * i <= maxNum; ++i) {
+		// i가 소수인 경우
+        if (isPrime[i]) {
+			// i의 배수들을 모두 소수가 아니라고 표시
+            for (int j = i * i; j <= maxNum; j += i) 
+            {
+                isPrime[j] = false;
+            }
+        }
     }
-    return true;
+	// 소수 여부를 담은 벡터 반환
+    return isPrime;
 }
 
 int main()
@@ -23,19 +35,16 @@ int main()
 	cin >> M;
 	cin >> N;
 
-    vector<int> primes;
+	// N까지의 소수 여부를 미리 계산
+	vector<bool> isPrime = sieveOfEratosthenes(N);
 
-    for (int i = M; i <= N; i++)
+	// M부터 N까지의 소수 출력
+	for (int i = M; i <= N; ++i) 
     {
-        if(isPrime(i))
+        if (isPrime[i]) 
         {
-            primes.push_back(i);
-		}
-    }
-
-    for(int i = 0; i < primes.size(); i++)
-    {
-        cout << primes[i] << "\n";
+            cout << i << "\n";
+        }
 	}
 
     return 0;
