1885b2b05e789f03bbaa054bdf2f53a542eeb758
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-02-12 04:54:51 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -12,40 +11,33 @@ void program()
 	int N;
 	cin >> N;
 
-	vector<int> R(N), G(N), B(N);
+	// 첫 번째 집의 비용 입력
+	int r, g, b;
+	cin >> r >> g >> b;
 
-	// dp(인덱스, 색깔) = 최소비용
-	// 최소값을 data로 가짐
-	// N번째 집의 색깔 RGB를 0,1,2로 표현
-	vector<vector<int>> dp(N,vector<int>(3, -1));
+	// 이전 집의 최소 비용 저장 변수
+	int prevR = r;
+	int prevG = g;
+	int prevB = b;
 
-	for (int i = 0; i < N; i++)
+	// 나머지 집들에 대해 비용 계산
+	for (int i = 1; i < N; i++)
 	{
-		cin >> R[i] >> G[i] >> B[i];
-	}
+		// 다음 집의 비용 입력
+		cin >> r >> g >> b;
 
-	// N번째 집은 N-1번쨰 집의 색과 다르게 칠해야 한다.
-	// dp[i][c] = i번째 집을 c색을 칠할 때의 최소 비용
-	for (int i = 0; i < N; i++)
-	{
-		// 첫번째 집은 그냥 칠함
-		if (i == 0)
-		{
-			dp[i][0] = R[i];
-			dp[i][1] = G[i];
-			dp[i][2] = B[i];
-		}
-		else
-		{
-			// 이전 집의 색깔과 다르게 칠함
-			dp[i][0] = R[i] + min(dp[i - 1][1], dp[i - 1][2]);
-			dp[i][1] = G[i] + min(dp[i - 1][0], dp[i - 1][2]);
-			dp[i][2] = B[i] + min(dp[i - 1][0], dp[i - 1][1]);
-		}
-	}
+		// 현재 집의 최소 비용 계산
+		int curR = r + min(prevG, prevB);
+		int curG = g + min(prevR, prevB);
+		int curB = b + min(prevR, prevG);
 
-	// 마지막 집까지 칠했을 때의 최소 비용
-	cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]}) << "\n";
+		// 이전 집의 비용 갱신
+		prevR = curR;
+		prevG = curG;
+		prevB = curB;
+	}
+	// 전체 집의 최소 비용 출력
+	cout << min({ prevR,prevG, prevB }) << '\n';
 }
 
 
