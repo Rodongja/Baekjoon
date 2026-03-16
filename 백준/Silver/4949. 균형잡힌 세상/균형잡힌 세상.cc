b74dba4f93215076ba97511b9513d8d5e7b18365
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-03-17 07:43:22 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,63 +1,89 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-// 균형잡힌 세계
+// 균형잡힌 세상
 
-void VPS()
+void check(string str)
 {
-    string str = "";
-	// 첫 번째 문자열 입력
-    getline(cin, str);
+	stack<char> s;
 
-    while (str != ".")
-    {
+	for (int i = 0; i < str.size(); i++)
+	{
+		// (,{,[가 들어가면 push
+		if (str[i] == '(' || str[i] == '[')
+		{
+			s.push(str[i]);
+		}
 
-        stack<char> s;
-        bool ok = true;
+		// ),},]인 경우
+		else if (str[i] == ')' || str[i] == ']')
+		{
+			// 여는 괄호가 없는데 닫는 괄호가 나온 경우
+			if (s.size() == 0)
+			{
+				cout << "no" << "\n";
+				return;
+			}
 
-        for (char c : str)
-        {
-            // 스택에 '(', '[' 입력
-            if (c == '(' || c == '[')
-            {
-                s.push(c);
-            }
-			// 스택에서 짝 맞추기
-			else if (c == ')')
-            {
-				// 짝이 맞지 않으면 ok = false
-                if (s.empty() || s.top() != '(')
-                {
-                    ok = false;
-                    break;
-                }
-                s.pop();
-            }
-            else if (c == ']')
-            {
-                if (s.empty() || s.top() != '[')
-                {
-                    ok = false;
-                    break;
-                }
-                s.pop();
-            }
-        }
+			// 미스매칭
+			else if (s.top() == '(' && str[i] != ')')
+			{
+				cout << "no" << "\n";
+				return;
+			}
 
-		// 스택이 비어있지 않으면 짝이 맞지 않음
-        if (!s.empty()) ok = false;
+			else if (s.top() == '[' && str[i] != ']')
+			{
+				cout << "no" << "\n";
+				return;
+			}
 
-        cout << (ok ? "yes\n" : "no\n");
+			// 나머지의 경우 마지막 괄호를 지운다
+			else
+			{
+				s.pop();
+			}
+		}
+	}
 
-		// 다음 문자열 입력
-        getline(cin, str);
-    }
+	// 검사 후 스택이 빈 경우
+	if (s.empty())
+	{
+		cout << "yes" << "\n";
+	}
+	// 스택이 남아있는 경우
+	else
+	{
+		cout << "no" << "\n";
+	}
 }
+
+void program()
+{
+	string str;
+	
+	while (str != ".")
+	{
+		getline(cin, str);
+		
+		if (str == ".")
+		{
+			break;
+		}
+
+		check(str);
+	}
+}
+
 
 int main()
 {
-    VPS();
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	program();
 	return 0;
 }
