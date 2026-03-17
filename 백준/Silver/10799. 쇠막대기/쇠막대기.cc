85ead0e7bd64b6e74502ac0fae664f8fd6d698c6
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-03-17 10:12:09 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -12,26 +12,31 @@ void program()
 	string str;
 	cin >> str;
 
+	// 스택
 	stack<char> s;
+	// 이전 문자 저장
 	char last = ' ';
+	// 쇠 막대기 조각 수
 	int bar = 0;
 
 	for (int i = 0; i < str.size(); i++)
 	{
 		s.push(str[i]);
 
-		// 쇠 막대기는 스택만큼 조각 추가
 		if (str[i] == ')' && last == '(')
 		{
+			// ()는 레이저 이므로 스택에서 () 제거
 			s.pop();
 			s.pop();
 
+			// 쇠 막대기는 스택의 길이만큼 조각 추가
 			bar += s.size();
 		}
 
 		// 쇠 막대기가 끝나는 부분이므로 1조각 추가
 		else if (str[i] == ')' && last == ')')
 		{
+			// ()는 막대기 이므로 스택에서 () 제거한 후 1조각 추가
 			s.pop();
 			s.pop();
 			bar++;			
