86092c901f4bf3a8f902b390beb3b89eb90de4db
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-05 19:53:51 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,45 +1,48 @@
 #include <iostream>
+#include <queue>
 #include <vector>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-// N과 M  (1)
-// 재귀와 dfs로 해결 가능
+// N과 M(1)
+// 백트래킹
 
-void dfs(int N, int M,vector<int>& vec, vector<bool>& visited)
+// 수열이 모이자마자 출력하기 때문에 따로 저장하지 않아도 됨
+void dfs(int N, int M, vector<int> vec, vector<bool> isUsed, int k)
 {
-	// 종료 조건
-	if (vec.size() == M)
+	// 길이가 M인 수열을 구했을 때 출력한다
+	if (k == M)
 	{
-		// 출력
 		for (int i = 0; i < M; i++)
 		{
-			cout << vec[i] << ' ';
+			cout << vec[i] << " ";
 		}
-		cout << '\n';
+
+		cout << "\n";
 		return;
 	}
 
-	// 재귀 호출
-	for(int i = 1; i <= N; i++)
+	// 1부터 N까지의 숫자 중에서 수열에 들어갈 숫자를 고른다
+	for (int i = 1; i < N + 1; i++)
 	{
-		// 이미 방문한 노드라면 패스
-		if (visited[i])
+		// 이미 방문한 숫자는 건너뛰기
+		if (!isUsed[i])
 		{
-			continue;
+			// 벡터에 숫자 추가
+			vec[k] = i;
+
+			// 방문 표시
+			isUsed[i] = true;
+
+			// 다음 숫자 추가
+			dfs(N,M,vec,isUsed,k + 1);
+
+			// 기존 dfs가 끝나면 방문 표시 해제
+			isUsed[i] = false;
 		}
-
-		// 방문 처리
-		visited[i] = true;
-		// 현재 노드 추가
-		vec.push_back(i);
-
-		// 재귀 호출
-		dfs(N, M, vec, visited);
-
-		// 백트래킹
-		vec.pop_back();
-		// 방문 처리 해제
-		visited[i] = false;
 	}
 }
 
@@ -47,11 +50,16 @@ void program()
 {
 	int N, M;
 	cin >> N >> M;
+	vector<int> vec(M);
+	vector<bool> isUsed(N + 1, false);
 
-	vector<int> vec;
-	vector<bool> visited(N + 1, false);
+	// 중복 없이 길이 M인 수열을 구하려면 N이 M보다 커야한다
+	if (M > N)
+	{
+		return;
+	}
 
-	dfs(N, M,vec,visited);
+	dfs(N,M,vec,isUsed,0);
 }
 
 
