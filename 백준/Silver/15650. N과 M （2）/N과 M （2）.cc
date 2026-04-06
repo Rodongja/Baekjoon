53278ca2c84fd9a9c82f2938a2cf215db9370102
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-07 01:36:32 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,35 +1,40 @@
 #include <iostream>
+#include <queue>
 #include <vector>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-// N과 M  (2)
-// 재귀와 dfs로 해결 가능
+// N과 M(2)
+// 백트래킹
 
-void dfs(int N, int M,vector<int>& vec, int start)
+// 수열이 모이자마자 출력하기 때문에 따로 저장하지 않아도 됨
+// min은 이번 단계에서 선택 가능한 최소 숫자
+// cur는 현재 검색중인 숫자
+void dfs(int N, int M, vector<int> vec, int min, int cur)
 {
-	// 종료 조건
-	if (vec.size() == M)
+	// 길이가 M인 수열을 구했을 때 출력한다
+	if (cur == M)
 	{
-		// 출력
 		for (int i = 0; i < M; i++)
 		{
-			cout << vec[i] << ' ';
+			cout << vec[i] << " ";
 		}
-		cout << '\n';
+
+		cout << "\n";
 		return;
 	}
 
-	// 재귀 호출
-	for(int i = start + 1; i <= N; i++)
+	// min부터 N까지의 숫자 중에서 수열에 들어갈 숫자를 고른다
+	for (int i = min; i < N + 1; i++)
 	{
-		// 현재 노드 추가
-		vec.push_back(i);
+		// 벡터에 숫자 추가
+		vec[cur] = i;
 
-		// 재귀 호출
-		dfs(N, M, vec, i);
-
-		// 백트래킹
-		vec.pop_back();
+		// 다음 숫자 추가
+		dfs(N, M, vec, i + 1, cur + 1);
 	}
 }
 
@@ -37,10 +42,15 @@ void program()
 {
 	int N, M;
 	cin >> N >> M;
+	vector<int> vec(M);
 
-	vector<int> vec;
-	int start = 0;
-	dfs(N, M,vec,start);
+	// 중복 없이 길이 M인 수열을 구하려면 N이 M보다 커야한다
+	if (M > N)
+	{
+		return;
+	}
+
+	dfs(N,M,vec,1,0);
 }
 
 
