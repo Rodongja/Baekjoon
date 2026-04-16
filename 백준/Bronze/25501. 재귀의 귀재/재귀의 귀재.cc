9b82a9aaec81329427de41b151b0117294ba5125
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-17 05:42:36 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -6,16 +6,19 @@
 using namespace std;
 
 // 재귀의 귀재
+// 전역변수 사용한 경우
 
-int recursion(const char* s, int l, int r, int& cnt) {
+int cnt;
+
+int recursion(const char* s, int l, int r) {
 	cnt++;
 	if (l >= r) return 1;
 	else if (s[l] != s[r]) return 0;
-	else return recursion(s, l + 1, r - 1, cnt);
+	else return recursion(s, l + 1, r - 1);
 }
 
-int isPalindrome(const char* s, int& cnt) {
-	return recursion(s, 0, strlen(s) - 1, cnt);
+int isPalindrome(const char* s) {
+	return recursion(s, 0, strlen(s) - 1);
 }
 
 
@@ -27,13 +30,13 @@ void program()
 
 	for (int i = 0; i < T; i++)
 	{
-		int cnt = 0;
+		cnt = 0;
 
 		string str;
 		cin >> str;
 		const char* c = str.c_str();
 
-		cout << isPalindrome(c,cnt) << " " << cnt << "\n";
+		cout << isPalindrome(c) << " " << cnt << "\n";
 	}
 }
 
