fd3e36e0e57558b54b6f3ad937cfae52a8bb593e
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-19 23:32:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -14,8 +14,10 @@ void program()
 	// 대칭성을 이용한 오버플로우 방지
 	if (K > N - K) K = N - K;
 
+	// i를 0으로 나누면 안되기 때문에 1부터 시작
 	for(int i = 1; i <= K; i++)
 	{
+		// (iN​)=(i−1N​)×iN−i+1​
 		result = result * (N - i + 1) / i;
 	}
 
