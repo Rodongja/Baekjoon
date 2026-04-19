6c701a54f498c3038152e1a46b5e1e26da0a57d8
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-19 23:32:47 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -5,20 +5,26 @@ using namespace std;
 // 팩토리얼
 // 재귀나 for로 해결가능
 
-int factorial(int num)
-{
-	if (num <= 1)
-		return 1;
-	
-	return num * factorial(num - 1);
-}
+//int factorial(int num)
+//{
+//	if (num <= 1)
+//		return 1;
+//	
+//	return num * factorial(num - 1);
+//}
 
 void program()
 {
 	int N;
 	cin >> N;
 
-	cout << factorial(N) << '\n';
+	int num = 1;
+	for (int i = 0; i < N; i++)
+	{
+		num *= (i + 1);
+	}
+
+	cout << num << "\n";
 }
 
 
