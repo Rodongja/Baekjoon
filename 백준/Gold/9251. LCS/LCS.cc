260fbb3b913c1213b654e0f41aaaf2b8ed54e678
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-19 23:30:11 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -12,28 +12,31 @@ void program()
 	string A, B;
 	cin >> A >> B;
 
-	int LCS = 0;
 	vector<vector<int>> dp(A.size() + 1, vector<int>(B.size() + 1, 0));
 
-	for(int i = 0; i < A.size(); i++)
+	// 0행과 0열은 빈 문자열을 의미하기 때문에 1열부터 사용한다
+	// A.size() + 1, B.size() + 1로 초기화 하기 때문에 범위를 벗어나지 않는다
+	// 결국 A.size()만큼의 행과 B.size()만큼의 열이 생긴다
+	for (int i = 0; i < A.size(); i++)
 	{
-		for(int j = 0; j < B.size(); j++)
+		for (int j = 0; j < B.size(); j++)
 		{
-			if(A[i] == B[j])
+			// 중복된 글자가 있으면
+			if (A[i] == B[j])
 			{
-				dp[i + 1][j + 1] = dp[i][j] + 1;
-				LCS = max(LCS, dp[i + 1][j + 1]);
-				continue;
+				// 기존 dp[i][j]에 1을 더한 값을 dp[i+1][j+1]에 저장한다
+				dp[i+1][j+1] = dp[i][j] + 1;
 			}
 			else
 			{
-				dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
-				continue;
+				// 중복된 글자가 없으면, A의 글자와 B의 글자 중 하나를 제외한 LCS 길이 중 최대값을 선택한다
+				dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
 			}
 		}
 	}
-	 
-	cout << LCS << "\n";
+
+	// A와 B의 최장 공통 부분 수열의 길이는 가장 마지막 글자까지의 LCS 길이를 구하면 된다
+	cout << dp[A.size()][B.size()] << "\n";
 }
 
 
