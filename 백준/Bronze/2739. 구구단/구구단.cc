4e508fb4fbb5b05b77349b2936173da344865067
MurlocGuy <96076144+Rodongja@users.noreply.github.com>
2026-04-19 23:48:45 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int a;
+	std::cin >> a;
+
+	for (int i = 1; i < 10; ++i)
+	{
+		std::cout << a << " * " << i << " = " << a * i << std::endl;
+	}
+	return 0;
+}
